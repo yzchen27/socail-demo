ab4c1a30acb44e917bf3337a46daf41f0d5aa04c
yzchen <engyzchen@foxmail.com>
2022-04-09 20:19:56 +0800

    feat:验证码接入

---
+++ tanhua-app-server/src/main/java/com/tanhua/server/AppServerApplication.java
@@ -0,0 +1,16 @@
+package com.tanhua.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author yzchen
+ */
+@SpringBootApplication
+public class AppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerApplication.class,args);
+    }
+}
